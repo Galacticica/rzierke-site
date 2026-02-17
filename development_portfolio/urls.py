9bd2127085311bcbfd5dce7a95b0f96f10d7f0ae
Galacticica <ojthecat127@gmail.com>
2026-02-17 07:48:26 -0600

    feat: add Resources view and template for managing development resources

--- development_portfolio/urls.py
+++ development_portfolio/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('', views.DevHomeView.as_view(), name='dev-home'),
     path('projects', views.DevPortfolioListView.as_view(), name='dev-project-list'),
     path('projects/<slug:slug>/', views.DevProjectDetailView.as_view(), name='dev-project-detail'),
+    path('resources/', views.ResourcesView.as_view(), name='dev-resources'),
 ]
